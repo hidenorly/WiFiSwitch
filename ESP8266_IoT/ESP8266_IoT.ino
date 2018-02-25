e93e05843b870c00d46961fa4bbbcc374cf961ae
hidenorly <twitte.harold@gmail.com>
2018-02-25 21:49:08 +0900

    Initial commit

---
+++ ESP8266_IoT/ESP8266_IoT.ino
@@ -0,0 +1,112 @@
+/* 
+ Copyright (C) 2016, 2018 hidenorly
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+extern "C" {
+#include "user_interface.h"
+}
+
+#include "base.h"
+#include "config.h"
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include "WiFiUtil.h"
+#include "WebConfig.h"
+#include "NtpUtil.h"
+#include "LooperThreadTicker.h"
+#include "OtaManager.h"
+
+#include <FS.h>
+#include <Time.h>
+#include <TimeLib.h>
+#include <NTP.h>
+
+#define DEBUG_PRINT(...) Serial.print(__VA_ARGS__)
+#define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__)
+
+// --- UPnP Device
+#include "Ssdp.h"
+#include "UPnPDeviceWiFiSwitch.h"
+
+static Ssdp* g_pSsdp=NULL;
+static UPnPDevice* g_pSwitch1=NULL;
+
+// --- mode changer
+bool initializeProperMode(bool bSPIFFS){
+  if( !bSPIFFS || (digitalRead(MODE_PIN) == 0) || (!SPIFFS.exists(WIFI_CONFIG))){
+    // setup because WiFi AP mode is specified or WIFI_CONFIG is not found.
+    setupWiFiAP();
+    setup_httpd();
+    return false;
+  } else {
+    setupWiFiClient();
+    setup_httpd();  // comment this out if you don't need to have httpd on WiFi client mode
+  }
+  return true;
+}
+
+// --- handler for WiFi client enabled
+void onWiFiClientConnected(){
+  DEBUG_PRINTLN("WiFi connected.");
+  DEBUG_PRINT("IP address: ");
+  DEBUG_PRINTLN(WiFi.localIP());
+  start_NTP(); // socket related is need to be executed in main loop.
+
+  if(g_pSsdp){}
+    g_pSwitch1 = new UPnPDeviceWiFiSwitch("light", 31415, "Belkin:device:**", "38323636-4558-4dda-9188-cda0e6aabbcc", "/setup.xml");
+    g_pSwitch1->enable(true);
+    g_pSsdp->addUPnPDevice(g_pSwitch1);
+    g_pSsdp->enable(true);
+}
+
+
+// --- General setup() function
+void setup() {
+  // Initialize GPIO
+  initializeGPIO();
+  
+  // Initialize SerialPort
+  Serial.begin(115200);
+
+  // Initialize SPI File System
+  bool bSPIFFS = SPIFFS.begin();
+  if(!bSPIFFS){
+    SPIFFS.format();
+  }
+
+  // Check mode
+  delay(1000);
+  if(initializeProperMode(bSPIFFS)){
+
+      g_pSsdp = new Ssdp();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  handleWiFiClientStatus();
+  handleWebServer();
+  g_LooperThreadManager.handleLooperThread();
+  static OtaManager ota(OTA_PIN, OTA_PIN_PERIOD);
+  ota.handle();
+  if(g_pSsdp && g_pSsdp->getEnabled()){
+    g_pSsdp->handle();
+  }
+  if(g_pSwitch1 && g_pSwitch1->getEnabled()){
+    g_pSwitch1->handle();
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # WiFiSwitch
+
 WiFi switch for Alexa/Home, etc.
+
+This is consisted of SSDP, UPnP base class.
+And as an example of the delived class, there is WiFi switch.
+
+Currently, for debug/experimental usage, it's implemented to emulate existing devices.
+
+# Dependencies
+
+## General steps to include zip library
+
+On Arduino IDE,
+
+1. Sketch
+2. Include library
+3. Install Zip library
+4. Specify the following zip-ed libraries.
+
+## Time library (by PaulStoffregen-san)
+
+```
+$ git clone https://github.com/PaulStoffregen/Time.git Time
+$ cd Time
+$ git archive HEAD --output=../Time.zip
+```
+
+## NTP library (by exabugs-san)
+
+```
+$ git clone https://github.com/exabugs/sketchLibraryNTP NTP
+$ cd NTP
+$ git archive HEAD --output=../NTP.zip
+```
